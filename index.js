038dc15c2a199e7ddbb8b9254dcb4ae4b8c48b9a
Fakhry <fakhry.linux@gmail.com>
2023-11-04 10:48:52 +0800

    Menghubungkan Mongoose Ke Mongodb
    npm init -y
    npm install mongoose@7
    nodemon index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/movie_db').then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.log(err);
+});
